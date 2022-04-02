8e7f9720d13ba4ad9f7ad433530d36e00843fc7a
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-04-02 15:44:03 +0900

    hi
    hi

--- line detection.py
+++ line detection.py
@@ -1,10 +1,6 @@
 import cv2
-import sys
 import numpy as np
 import pre_function as pf
-
-
-
 
 
 video = cv2.VideoCapture('실선 차선인식 test.mp4')
@@ -16,9 +12,16 @@ threshold = 90
 min_line_len = 120
 max_line_gap = 150
 
-def line_detectio(src):
+def line_detection(src):
         lined = pf.hough_lines(src, rho, theta, threshold, min_line_len, max_line_gap)
         return lined
+    
+def pre_processing(src):
+    frame = cv2.resize(src,(960,720))
+    pre_frame = pf.pre(frame)
+    edge_frame = pf.edge(pre_frame)
+    masked = pf.making_mask(edge_frame,frame)
+    return masked
 
 while True :
     ret, frame = video.read()
@@ -26,11 +29,11 @@ while True :
     if not ret:
         break
     
-    pre_processing_video = pf.pre_processing(frame)
-    lined_video = line_detectio(pre_processing_video)
+    pre_processing_video = pre_processing(frame)
+    lined_video = line_detection(pre_processing_video)
     final = cv2.addWeighted(lined_video, 1., frame, 1., 0. )
 
-   
+    cv2.imshow('lined_video', lined_video)
     cv2.imshow('frame', frame)
     cv2.imshow('dst', final)
 
